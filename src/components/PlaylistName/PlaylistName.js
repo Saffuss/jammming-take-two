c9d8c40d92d4735ced31cbef9105fd1fc0954da9
saffuss <smithfuss@gmail.com>
2024-07-05 15:32:32 -0600

    Add query declaration and handleInputChange declaration

--- src/components/PlaylistName/PlaylistName.js
+++ src/components/PlaylistName/PlaylistName.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function PlaylistName() {
+    const [query, setQuery] = useState('')
+
+    function handleInputChange(event) {
+        setQuery(event.target.value);
+    }
+
     return (
         <>
-            <input type='text' placeholder='playlist name'/>
+            <input
+            type='text'
+            placeholder='playlist name'
+            value={query}
+            onChange={handleInputChange}
+            />
         </>
     );
 }
