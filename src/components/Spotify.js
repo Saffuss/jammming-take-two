7a300a3db0118529224673d42e6016a37df6e180
saffuss <smithfuss@gmail.com>
2024-07-19 16:56:42 -0600

    Add Spotify.getAccessToken() method for getting the access token from the url that Spotify sends

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -1,5 +1,27 @@
-const Spotify = {
+const client_id = '2bb296eed39c4be7bfd5ef153ef3097e';
+const redirect_uri = 'http://localhost:3000/';
+let accessToken;
+let expiresIn;
 
+const Spotify = {
+    getAccessToken() {
+        if (accessToken) {
+            return accessToken;
+        }
+
+        const hash = window.location.hash;
+        if (hash) {
+            const params = new URLSearchParams(hash.substring(1));
+            accessToken = params.get('access_token');
+            expiresIn = params.get('expires_in');
+            window.setTimeout(() => accessToken='', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${client_id}&redirect_uri=${redirect_uri}`;
+            window.location = accessUrl;
+        }
+    }
 }
 
 export default Spotify;
