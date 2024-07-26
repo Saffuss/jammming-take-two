ef40c67b41bbc5909f0acfd225cb38fc2c4f3542
saffuss <smithfuss@gmail.com>
2024-07-26 14:45:59 -0600

    More css stuff

--- src/components/UserPlaylist/UserPlaylist.js
+++ src/components/UserPlaylist/UserPlaylist.js
@@ -3,9 +3,9 @@ import "./UserPlaylist.modules.css";
 
 function UserPlaylist(props) {
     return (
-        <div className='playlist'>
+        <div className='userPlaylist'>
             <h3 className="h3">{props.playlist.name}</h3>
-            <p className="p">{props.playlist.tracks}</p>
+            <p className="p">{/*props.playlist.tracks*/}</p>
         </div>
     );
 }
